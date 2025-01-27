ce8a5e78efe5ad37e108bd736787f240544730a6
Khaiqol <Khaiqol@gmail.com>
2025-01-27 15:03:01 +0700

    memperbaiki update aktivitas view dan view model nya

--- app/src/main/java/com/example/a10_115/ui/view/aktivitasPertanian/UpdateAktivitasView.kt
+++ app/src/main/java/com/example/a10_115/ui/view/aktivitasPertanian/UpdateAktivitasView.kt
@@ -1,2 +1,68 @@
 package com.example.a10_115.ui.view.aktivitasPertanian
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a10_115.ui.Navigation.DestinasiNavigasi
+import com.example.a10_115.ui.costumWidget.CostumeTopAppBar
+import com.example.a10_115.ui.viewModel.PenyediaViewModel
+import com.example.a10_115.ui.viewModel.aktivitasPertanian.UpdateAktivitasPertanianViewModel
+import com.example.a10_115.ui.viewModel.pekerja.HomePekerjaViewModel
+import com.example.a10_115.ui.viewModel.tanaman.HomeTanamanViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiUpdateAktivitasPertanian : DestinasiNavigasi {
+    override val route = "update_aktivitas"
+    override val titleRes = "Update Data aktivitas"
+    const val idAktivitas = "idAktivitas"
+    val routeWithArgs = "$route/{$idAktivitas}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateAktivitasView(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,// Navigasi kembali ke HomePekerja
+    modifier: Modifier = Modifier,
+    viewModel: UpdateAktivitasPertanianViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    viewModelPekerja: HomePekerjaViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    viewModelTanaman: HomeTanamanViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiUpdateAktivitasPertanian.titleRes,
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiaktivitasPertanianState,
+            onAktivitasPertanianValueChange = viewModel::updateInsertAktivitasPertanianState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateAktivitasPertanian() // Menyimpan data pekerja
+                    navigateBack() // Navigasi kembali ke HomePekerja
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
--- app/src/main/java/com/example/a10_115/ui/viewModel/aktivitasPertanian/UpdateAktivitasViewModel.kt
+++ app/src/main/java/com/example/a10_115/ui/viewModel/aktivitasPertanian/UpdateAktivitasViewModel.kt
@@ -1,2 +1,42 @@
 package com.example.a10_115.ui.viewModel.aktivitasPertanian
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a10_115.repository.AktivitasPertanianRepository
+import com.example.a10_115.ui.view.aktivitasPertanian.DestinasiUpdateAktivitasPertanian
+import kotlinx.coroutines.launch
+
+class UpdateAktivitasPertanianViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val atv: AktivitasPertanianRepository
+) : ViewModel() {
+
+    var uiaktivitasPertanianState by mutableStateOf(InsertUiState())
+        private set
+
+    val idAktivitas: String = checkNotNull(savedStateHandle[DestinasiUpdateAktivitasPertanian.idAktivitas])
+
+    init {
+        viewModelScope.launch {
+            uiaktivitasPertanianState = atv.getAktivitasPertanianById(idAktivitas).toUiStateAktivitasPertanian()
+        }
+    }
+
+    fun updateInsertAktivitasPertanianState(insertUiEvent: InsertUiEvent) {
+        uiaktivitasPertanianState = uiaktivitasPertanianState.copy(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun updateAktivitasPertanian(){
+        viewModelScope.launch {
+            try {
+                atv.updateAktivitasPertanian(idAktivitas, uiaktivitasPertanianState.insertUiEvent.toAktivitasPertanian())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
